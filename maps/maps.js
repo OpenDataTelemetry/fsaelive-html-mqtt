d3555d3b2f58d56f8eead3aa41fb0fc6982a7e75
Pedro Correia <pedro.tukinho@gmail.com>
2024-08-02 17:35:39 -0300

    Create and update all the cars

--- maps/maps.js
+++ maps/maps.js
@@ -14,31 +14,84 @@ function changeMap(mapName) {
     imageUrl = `src/${mapName}.png`,
         image.setUrl(imageUrl)
 }
-const carPosition = {}
-function createCircles(lat, long) {
-    var circle = L.circle([lat, long], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 10
+
+const icCarsPosition = new Map()
+for (let i = 0; i < icCars.length; i++) {
+    icCarsPosition.set(icCars[i][0], createCircles(colors[i], icCars[i][1]))
+}
+const elCarsPosition = new Map() 
+for (let i = 0; i < elCars.length; i++) {
+    elCarsPosition.set(elCars[i][0], createRectangles(colors[i], elCars[i][1]))
+}
+
+function createPopup(teamName){
+    var popup = L.popup(
+    {
+        content: `<p>${teamName}</p>`,
+        closeButton: false,
+        autoClose: false,
+        autoPan : false,
+        keepInView: true,
+        minWidth: 0,
+        maxWidth: 30,
+        maxHeight: 50
+    })
+    return popup;
+}
+
+function createCircles(color, teamName) {
+    var popup = createPopup(teamName);
+    var circle = L.circle([0, 0], {
+        color: color,
+        opacity: 0.5,
+        fillOpacity: 1,
     }).addTo(map);
+    circle.bindPopup(popup).openPopup();
     return circle;
 }
 
+function createRectangles(color, teamName){
+    var popup = createPopup(teamName);
+    var rectangle = L.rectangle([[0,0],[0,0]], {
+        color: color,
+        opacity: 0.5,
+        fillOpacity: 1,
+        maxWidth: 100,
+    }).addTo(map);
+    rectangle.bindPopup(popup).openPopup();
+    return rectangle;
+}
+
+
+function updateCircles(circle, lat, long){
+    circle.setLatLng([lat,long]);
+}
+
+function updateRectangles(rectangle, lat, long){
+    const rectangleSize = [0.00018, 0.0002];
+    var topLeftBound = [lat + rectangleSize[0] / 2, long - rectangleSize[1]/2];
+    var bottomRightBound = [lat - rectangleSize[0]/2, long + rectangleSize[1]/2];
+    rectangle.setBounds([topLeftBound, bottomRightBound]);
+}
+
 // Update the position on the map
-function updatePosition(latitude, longitude){
-    console.log(latitude)
-    console.log(longitude)
-    if (carPosition.position != undefined) carPosition.position.remove();
-    carPosition.position = createCircles(latitude, longitude);
-    console.log(carPosition)
+function updatePosition(latitude, longitude, topic){    
+    let carType = String(topic.split("/").at(-2));
+
+    if(carType.startsWith('E') && elCarsPosition.has(topic)){
+        updateRectangles(elCarsPosition.get(carType), latitude, longitude)
+    }
+    else if(icCarsPosition.has(topic)){
+        updateCircles(icCarsPosition.get(carType), latitude, longitude)
+    }
 }
 
 // The handling of the received message
 function receiveSignal(message) {
     var data = JSON.parse(message.payloadString)
     if (data._type == "location") {
-        updatePosition(data.lat, data.lon)
+        var topic = message.destinationName
+        updatePosition(data.lat, data.lon, topic)
     }
 }
 
